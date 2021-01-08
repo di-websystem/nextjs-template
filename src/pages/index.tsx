41265a01bfb00afb6d743c0586c018711856f27b
tatsuo <you@test.com>
2021-01-08 11:23:24 +0900

    add: fetch-lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,32 @@
 import { NextPage } from 'next';
 import styled from 'styled-components';
+import useSWR from 'swr';
 
 const Title = styled.h1`
   color: red;
   font-size: 18px;
 `;
 
+const fetcher = (url: string) => {
+  return fetch(url)
+    .then((res) => res.json())
+    .catch((error) => error);
+};
+
 const Index: NextPage = () => {
-  return <Title>aaa</Title>;
+  const { data, error } = useSWR('/api/user', fetcher, {
+    revalidateOnFocus: false,
+  });
+
+  if (error) {
+    return <div>error</div>;
+  }
+
+  if (!data) {
+    return <div>loading...</div>;
+  }
+
+  return <Title>{data.name}</Title>;
 };
 
 export default Index;
